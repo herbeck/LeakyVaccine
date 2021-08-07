faf3dbea59e3f3faac7e3409f9488ae6b4b540d4
David Kong <dkong@idmod.org>
2021-08-07 14:26:54 -0700

    updated to fix runtime error when app is running in container.

--- Paul-visualization.R
+++ Paul-visualization.R
@@ -8,6 +8,26 @@ createVisualization <- function(output, reac) {
     fit <-runSim_Paul(reac);
   });
   
+  bounds <- list(#c(0.003, 0.008),      # beta
+    #c(60/365, 120/365))    # c
+    epsilon = c(1E-10, 1-1E-10),
+    #               lambda = c(0.000005, 0.0001),  # lambda
+    lambda = c(0.000005, 0.00001),  # lambda
+    risk = c(1, 20))            # risk multiplier
+  
+  
+  time <- 3*365; # End of the trial.
+  
+  ## Note here actually want to target the average incidence over time, rather than the incidence at the end of the trial, since with multiple risk groups there is waning expected. I have changed this in the ABC function f, below.
+  placebo.incidence.target <- rep( 3, length( time ) )    # flat incidence of 3.5% per 100 person years
+  
+  ### Paul notes this is not going to work, if efficacy is waning you can't match a constant efficacy over time. We should match just one time, probably duration of 702 trial.
+  VE.target <- rep(0.3, length( time ))
+  target.stats <- data.frame(time, VE.target, placebo.incidence.target )
+  
+  lower.bounds <- sapply( bounds, function ( .bounds.for.x ) { .bounds.for.x[ 1 ] } );
+  upper.bounds <- sapply( bounds, function ( .bounds.for.x ) { .bounds.for.x[ 2 ] } );
+  
   #------------------------------------------------------------------------------
   # for creating Lambda risk plot
   #------------------------------------------------------------------------------
