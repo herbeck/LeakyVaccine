d5d22286de4471feb1af181f88a8d91d1a3fd4e5
herbeck <joshherbeck@gmail.com>
2021-09-09 11:05:15 -0700

    Revising posterior density plots for RV144 and 702 scenarios

--- Plots.Sept8.R
+++ Plots.Sept8.R
@@ -10,6 +10,7 @@ HVTN702.VE.target <- sim.results$target.stats$hvtn702.VE.target
 lower.bounds <- sapply( bounds, function ( .bounds.for.x ) { .bounds.for.x[ 1 ] } );
 upper.bounds <- sapply( bounds, function ( .bounds.for.x ) { .bounds.for.x[ 2 ] } );
 
+# epsilon (per-exposure VE; true VE) (used for both RV144 and HVTN702 sims)
 plot(density(fit$param[, 1], from = lower.bounds[1],  to = upper.bounds[1]),
      main = "epsilon", 
      xlim = c(lower.bounds[1], upper.bounds[1]),
@@ -21,23 +22,35 @@ abline(v = HVTN702.VE.target/100, lty = 2, col = 4)
 legend("bottomright", legend = c("RV144 target VE", "HVTN702 target VE", "Posterior"),
        lty = c(2, 2, 1), col = c(2, 4, 1), lwd = 1, cex = 0.6)
 
+#### RV144 plots and 702 plots, overlaid
+
+# lambda (force of infection; beta*prev*c)
 plot(density(fit$param[, 2], from = lower.bounds[2],  to = upper.bounds[2]),
      main = "log10lambda", 
      xlim = c(lower.bounds[2], upper.bounds[2]),
-     col=2)
-lines(density(fit$param[, 2], from = lower.bounds[2],  to = upper.bounds[2]), col = 2)
-#abline(v = VE.target, lty = 2, col = 1) # This is a bug, it plots VE target, not lambda
-legend("topright", legend = c("Truth", "Posterior"),
-       lty = c(1, 2), col = 1:2, lwd = 2)
+     col=1)
+lines(density(fit$param[, 6], from = lower.bounds[6],  to = upper.bounds[6]), col = 2)
+legend("bottomright", legend = c("RV144", "HVTN702"),
+       lty = c(1, 2), col = 1:2, lwd = 1, cex = 0.75)
 
+# high risk multiplier
 plot(density(fit$param[, 3], from = lower.bounds[3],  to = upper.bounds[3]),
-     main = "risk", 
+     main = "risk multiplier", 
      xlim = c(lower.bounds[3], upper.bounds[3]),
-     col=2)
-lines(density(fit$param[, 3], from = lower.bounds[3],  to = upper.bounds[3]), col = 2)
-#abline(v = VE.target, lty = 2, col = 1) # Another bug
-legend("topright", legend = c("Truth", "Posterior"),
-       lty = c(1, 2), col = 1:2, lwd = 2)
+     col=1)
+lines(density(fit$param[, 7], from = lower.bounds[7],  to = upper.bounds[7]), col = 2)
+legend("bottomright", legend = c("RV144", "HVTN702"),
+       lty = c(1, 2), col = 1:2, lwd = 1, cex = 0.75)
+
+# high risk proportion
+plot(density(fit$param[, 4], from = lower.bounds[4],  to = upper.bounds[4]),
+     main = "high risk proportion", 
+     xlim = c(lower.bounds[4], upper.bounds[4]),
+     col=1)
+lines(density(fit$param[, 8], from = lower.bounds[8],  to = upper.bounds[8]), col = 2)
+legend("bottomright", legend = c("RV144", "HVTN702"),
+       lty = c(1, 2), col = 1:2, lwd = 1, cex = 0.75)
+
 
 .df <- as.data.frame( fit$param ) #df of just the parameter combinations ABC sampled
 names( .df ) <- c( "epsilon", "log10lambda", "risk" )
